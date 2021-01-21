af358271dd27413fd3d50bd0784747d85e29c8f2
SChickenMan <64627604+SChickenMan@users.noreply.github.com>
2021-01-21 00:46:23 -0500

    Update questions.js

--- questions.js
+++ questions.js
@@ -6,7 +6,7 @@ questions = [
             "plty": 0,
             "mode": 0,
             "thpr": 0,
-            "shty": 5,
+            "shty": 0,
             "ethi": 0
         }
     },
@@ -14,31 +14,20 @@ questions = [
         "question": "Oppression by corporations is more of a concern than oppression by governments.",
         "effect": {
             "gapl": 0,
-            "plty": 5,
-            "mode": -10,
-            "thpr": 5,
-            "shty": -5,
-            "ethi": 10
+            "plty": 10,
+            "mode": 0,
+            "thpr": 0,
+            "shty": 0,
+            "ethi": 0
             
         }
     },
     {
         "question": "Oppression by corporations is more of a concern than oppression by governments.",
         "effect": {
-            "gapl": -10,
-            "plty": 0,
-            "mode": 5,
-            "thpr": 5,
-            "shty": 0,
-            "ethi": 5
-        }
-    },
-    {
-        "question": "Oppression by corporations is more of a concern than oppression by governments.",
-        "effect": {
             "gapl": 0,
             "plty": 0,
-            "mode": 5,
+            "mode": 10,
             "thpr": 0,
             "shty": 0,
             "ethi": 0
@@ -47,67 +36,78 @@ questions = [
     {
         "question": "Oppression by corporations is more of a concern than oppression by governments.",
         "effect": {
-            "gapl": 10,
+            "gapl": 0,
+            "plty": 0,
+            "mode": 0,
+            "thpr": 10,
+            "shty": 0,
+            "ethi": 0
+        }
+    },
+    {
+        "question": "Oppression by corporations is more of a concern than oppression by governments.",
+        "effect": {
+            "gapl": 0,
             "plty": 0,
             "mode": 0,
             "thpr": 0,
             "shty": 10,
+            "ethi": 0
+        }
+    },
+    {
+        "question": "Oppression by corporations is more of a concern than oppression by governments.",
+        "effect": {
+            "gapl": 0,
+            "plty": 0,
+            "mode": 0,
+            "thpr": 0,
+            "shty": 0,
             "ethi": 10
         }
     },
     {
         "question": "Oppression by corporations is more of a concern than oppression by governments.",
         "effect": {
-            "gapl": 10,
-            "plty": 5,
-            "mode": -10,
-            "thpr": 10,
-            "shty": 5,
-            "ethi": -5
-        }
-    },
-    {
-        "question": "Oppression by corporations is more of a concern than oppression by governments.",
-        "effect": {
             "gapl": 0,
-            "plty": 10,
-            "mode": 10,
-            "thpr": 0,
-            "shty": -5,
-            "ethi": -10,
-        }
-    },
-    {
-        "question": "Oppression by corporations is more of a concern than oppression by governments.",
-        "effect": {
-            "gapl": 0,
-            "plty": 10,
+            "plty": 0,
             "mode": 0,
-            "thpr": -5,
-            "shty": 5,
+            "thpr": 0,
+            "shty": 0,
             "ethi": 0,
         }
     },
     {
         "question": "Oppression by corporations is more of a concern than oppression by governments.",
         "effect": {
-            "gapl": 10,
+            "gapl": 0,
             "plty": 0,
-            "mode": -10,
+            "mode": 0,
             "thpr": 0,
-            "shty": -10,
-            "ethi": 10
+            "shty": 0,
+            "ethi": 0,
         }
     },
     {
         "question": "Oppression by corporations is more of a concern than oppression by governments.",
         "effect": {
-            "gapl": 10,
+            "gapl": 0,
             "plty": 0,
-            "mode": 5,
+            "mode": 0,
             "thpr": 0,
-            "shty": 5,
-            "ethi": 10
+            "shty": 0,
+            "ethi": 0
+        }
+    },
+    {
+        "question": "Oppression by corporations is more of a concern than oppression by governments.",
+        "effect": {
+            "gapl": 0,
+            "plty": 0,
+            "mode": 0,
+            "thpr": 0,
+            "shty": 0,
+            "ethi": 0
         }
     },
     
