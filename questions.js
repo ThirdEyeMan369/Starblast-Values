4ec283e4d90ef7f8539cb8e6389725d8fd1e3e67
SChickenMan <64627604+SChickenMan@users.noreply.github.com>
2021-01-21 02:48:21 -0500

    Update questions.js

--- questions.js
+++ questions.js
@@ -1,6 +1,6 @@
 questions = [
     {
-        "question": ""Fuck mining" - Uranus is big, July 12th 2018",
+        "question": "I enjoy mining",
         "effect": {
             "gapl": -10,
             "plty": 0,
@@ -11,10 +11,10 @@ questions = [
         }
     },
     {
-        "question": "Oppression by corporations is more of a concern than oppression by governments.",
+        "question": "I usually get the most kills",
         "effect": {
-            "gapl": 0,
-            "plty": 10,
+            "gapl": 10,
+            "plty": 0,
             "mode": 0,
             "thpr": 0,
             "shty": 0,
@@ -23,10 +23,43 @@ questions = [
         }
     },
     {
-        "question": "Oppression by corporations is more of a concern than oppression by governments.",
+        "question": "People team in Starblast because that is just human nature",
+        "effect": {
+            "gapl": 0,
+            "plty": -10,
+            "mode": 0,
+            "thpr": 5,
+            "shty": 0,
+            "ethi": 0
+        }
+    },
+    {
+        "question": "I prefer to play modes that does not require teaming",
+        "effect": {
+            "gapl": 0,
+            "plty": 10,
+            "mode": 0,
+            "thpr": 0,
+            "shty": 0,
+            "ethi": 0
+        }
+    },
+    {
+        "question": "I prefer to play the primary modes of the game",
         "effect": {
             "gapl": 0,
             "plty": 0,
+            "mode": -10,
+            "thpr": 0,
+            "shty": 0,
+            "ethi": 0
+        }
+    },
+    {
+        "question": "I would like to see more to Modding Space",
+        "effect": {
+            "gapl": 0,
+            "plty": 10,
             "mode": 10,
             "thpr": 0,
             "shty": 0,
@@ -34,29 +67,18 @@ questions = [
         }
     },
     {
-        "question": "Oppression by corporations is more of a concern than oppression by governments.",
+        "question": "Strategy is very important",
         "effect": {
             "gapl": 0,
             "plty": 0,
             "mode": 0,
-            "thpr": 10,
+            "thpr": -10,
             "shty": 0,
             "ethi": 0
         }
     },
     {
-        "question": "Oppression by corporations is more of a concern than oppression by governments.",
-        "effect": {
-            "gapl": 0,
-            "plty": 0,
-            "mode": 0,
-            "thpr": 0,
-            "shty": 10,
-            "ethi": 0
-        }
-    },
-    {
-        "question": "Oppression by corporations is more of a concern than oppression by governments.",
+        "question": "Fast paced games are more enjoyable",
         "effect": {
             "gapl": 0,
             "plty": 0,
@@ -67,29 +89,40 @@ questions = [
         }
     },
     {
-        "question": "Oppression by corporations is more of a concern than oppression by governments.",
+        "question": "I enjoy using the O-Defender",
+        "effect": {
+            "gapl": 0,
+            "plty": 0,
+            "mode": 0,
+            "thpr": 0,
+            "shty": -10,
+            "ethi": 0,
+        }
+    },
+    {
+        "question": "Fast ships are fun to use",
+        "effect": {
+            "gapl": 0,
+            "plty": 0,
+            "mode": 0,
+            "thpr": 0,
+            "shty": 10,
+            "ethi": 0,
+        }
+    },
+    {
+        "question": "I usually say GG when I get killed by player or kill a player",
         "effect": {
             "gapl": 0,
             "plty": 0,
             "mode": 0,
             "thpr": 0,
             "shty": 0,
-            "ethi": 0,
+            "ethi": -10
         }
     },
     {
-        "question": "Oppression by corporations is more of a concern than oppression by governments.",
-        "effect": {
-            "gapl": 0,
-            "plty": 0,
-            "mode": 0,
-            "thpr": 0,
-            "shty": 0,
-            "ethi": 0,
-        }
-    },
-    {
-        "question": "Oppression by corporations is more of a concern than oppression by governments.",
+        "question": "Spamming emotes is fun",
         "effect": {
             "gapl": 0,
             "plty": 0,
@@ -100,16 +133,272 @@ questions = [
         }
     },
     {
-        "question": "Oppression by corporations is more of a concern than oppression by governments.",
+        "question": "In certain occasions, it is acceptable to crossteam in team mode",
+        "effect": {
+            "gapl": 0,
+            "plty": -5,
+            "mode": 0,
+            "thpr": 0,
+            "shty": 0,
+            "ethi": 10
+        }
+    },
+    {
+        "question": "I usually go off to do my own thing instead of sticking with the team",
+        "effect": {
+            "gapl": 0,
+            "plty": 10,
+            "mode": 0,
+            "thpr": 5,
+            "shty": 0,
+            "ethi": 0
+        }
+    },
+    {
+        "question": "The vanilla ship tree should be updated more often",
+        "effect": {
+            "gapl": 0,
+            "plty": 0,
+            "mode": -10,
+            "thpr": 0,
+            "shty": 0,
+            "ethi": 0
+        }
+    },
+    {
+        "question": "I have made a mod or would make a mod if I knew how to",
+        "effect": {
+            "gapl": 0,
+            "plty": 0,
+            "mode": 10,
+            "thpr": 0,
+            "shty": 0,
+            "ethi": 0
+        }
+    },
+    {
+        "question": "Battle Royale is a fun mod",
+        "effect": {
+            "gapl": 5,
+            "plty": 5,
+            "mode": 5,
+            "thpr": 10,
+            "shty": 0,
+            "ethi": 0
+        }
+    },
+    {
+        "question": "I like to discuss strategies among other players",
+        "effect": {
+            "gapl": 0,
+            "plty": 0,
+            "mode": 0,
+            "thpr": -10,
+            "shty": 0,
+            "ethi": -5
+        }
+    },
+    {
+        "question": "The A-Speedster meta should be revived",
+        "effect": {
+            "gapl": 0,
+            "plty": 0,
+            "mode": 0,
+            "thpr": 5,
+            "shty": 10,
+            "ethi": 0
+        }
+    },
+    {
+        "question": "The heavier the ship, the better",
+        "effect": {
+            "gapl": 0,
+            "plty": 0,
+            "mode": 0,
+            "thpr": 0,
+            "shty": -10,
+            "ethi": 0
+        }
+    },
+    {
+        "question": "Trolling in game is fun",
         "effect": {
             "gapl": 0,
             "plty": 0,
             "mode": 0,
             "thpr": 0,
             "shty": 0,
+            "ethi": 10
+        }
+    },
+    {
+        "question": "I usually help new players in game",
+        "effect": {
+            "gapl": 0,
+            "plty": -5,
+            "mode": 0,
+            "thpr": 0,
+            "shty": 0,
+            "ethi": -10
+        }
+    },
+    {
+        "question": "Fuck mining - Uranus is big, July 12th 2018",
+        "effect": {
+            "gapl": 10,
+            "plty": 0,
+            "mode": 0,
+            "thpr": 5,
+            "shty": 0,
             "ethi": 0
         }
     },
+    {
+        "question": "Powermining is something I do often",
+        "effect": {
+            "gapl": -10,
+            "plty": 0,
+            "mode": 0,
+            "thpr": 5,
+            "shty": 0,
+            "ethi": 0
+        }
+    },
+    {
+        "question": "I help my teamates mine when I am fully upgraded",
+        "effect": {
+            "gapl": -10,
+            "plty": -5,
+            "mode": 0,
+            "thpr": -10,
+            "shty": 0,
+            "ethi": -10
+        }
+    },
+    {
+        "question": "No matter what the cause is, I must get as many kills as possible",
+        "effect": {
+            "gapl": 10,
+            "plty": 0,
+            "mode": 0,
+            "thpr": 10,
+            "shty": 0,
+            "ethi": 10
+        }
+    },
+    {
+        "question": "My teamates should leave me alone",
+        "effect": {
+            "gapl": 0,
+            "plty": 10,
+            "mode": 0,
+            "thpr": 5,
+            "shty": 0,
+            "ethi": 5
+        }
+    },
+    {
+        "question": "I usually like the company of other players",
+        "effect": {
+            "gapl": 0,
+            "plty": -10,
+            "mode": 0,
+            "thpr": 0,
+            "shty": 0,
+            "ethi": -5
+        }
+    },
+    {
+        "question": "I would make a mod for Modding Space",
+        "effect": {
+            "gapl": 0,
+            "plty": 0,
+            "mode": 0,
+            "thpr": 0,
+            "shty": 0,
+            "ethi": -5
+        }
+    },
+    {
+        "question": "Modding Space was a mistake",
+        "effect": {
+            "gapl": 0,
+            "plty": 0,
+            "mode": -10,
+            "thpr": 0,
+            "shty": 0,
+            "ethi": 0
+        }
+    },
+    {
+        "question": "I am always cautious of what is on my radar",
+        "effect": {
+            "gapl": 0,
+            "plty": 0,
+            "mode": 0,
+            "thpr": -10,
+            "shty": 0,
+            "ethi": 0
+        }
+    },
+    {
+        "question": "I enjoy playing without a plan, and just play for the intensity",
+        "effect": {
+            "gapl": 5,
+            "plty": 0,
+            "mode": 0,
+            "thpr": 10,
+            "shty": 0,
+            "ethi": 0
+        }
+    },
+    
+    {
+        "question": "I like to put myself on the front line in battles",
+        "effect": {
+            "gapl": 0,
+            "plty": 0,
+            "mode": 0,
+            "thpr": 0,
+            "shty": -10,
+            "ethi": 0
+        }
+    },
+    {
+        "question": "Racing is fun",
+        "effect": {
+            "gapl": -5,
+            "plty": 5,
+            "mode": 5,
+            "thpr": 0,
+            "shty": 10,
+            "ethi": 0
+        }
+    },
+    {
+        "question": "A "screw off" emote should exist",
+        "effect": {
+            "gapl": 0,
+            "plty": 10,
+            "mode": 0,
+            "thpr": 10,
+            "shty": 0,
+            "ethi": 10
+        }
+    },
+    {
+        "question": "I sometimes join games just to stop people from trolling",
+        "effect": {
+            "gapl": 5,
+            "plty": 0,
+            "mode": 0,
+            "thpr": 5,
+            "shty": 0,
+            "ethi": -10
+        }
+    },
+    
+    
     
     
 ];
