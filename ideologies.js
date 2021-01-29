132b4760a2dcb23840904df49e19f560b69b00c7
SChickenMan <64627604+SChickenMan@users.noreply.github.com>
2021-01-29 00:57:16 -0500

    Update ideologies.js

--- ideologies.js
+++ ideologies.js
@@ -2,58 +2,246 @@ ideologies = [
     {
         "name": "Mining God",
         "stats": {
-            "gapl": 10,
-            "plty": 0,
-            "mode": -10,
-            "thpr": -10,
-            "shty": -10,
-            "ethi": 0
+            "gapl": 100,
+            "plty": 50,
+            "mode": 50,
+            "thpr": 50,
+            "shty": 100,
+            "ethi": 50
         }
     },
     {
         "name": "Killing God",
         "stats": {
             "gapl": 0,
-            "plty": 20,
-            "mode": 0,
-            "thpr": 10,
-            "shty": 10,
-            "ethi": 5
+            "plty": 50,
+            "mode": 50,
+            "thpr": 50,
+            "shty": 0,
+            "ethi": 50
         }
     },
     {
         "name": "Hiveminded",
         "stats": {
-            "gapl": 5,
-            "plty": 10,
-            "mode": 20,
-            "thpr": -20,
-            "shty": -10,
+            "gapl": 50,
+            "plty": 100,
+            "mode": 50,
+            "thpr": 50,
+            "shty": 50,
+            "ethi": 50
+        }
+    },
+    {
+        "name": "Isolationist",
+        "stats": {
+            "gapl": 50,
+            "plty": 0,
+            "mode": 50,
+            "thpr": 50,
+            "shty": 50,
+            "ethi": 50
+        }
+    },
+    {
+        "name": "Veteran",
+        "stats": {
+            "gapl": 50,
+            "plty": 50,
+            "mode": 100,
+            "thpr": 50,
+            "shty": 50,
+            "ethi": 50
+        }
+    },
+    {
+        "name": "Hackerman",
+        "stats": {
+            "gapl": 50,
+            "plty": 50,
+            "mode": 0,
+            "thpr": 50,
+            "shty": 50,
+            "ethi": 50
+        }
+    },
+    {
+        "name": "Big Brained",
+        "stats": {
+            "gapl": 50,
+            "plty": 50,
+            "mode": 50,
+            "thpr": 100,
+            "shty": 50,
+            "ethi": 50
+        }
+    },
+    {
+        "name": "Processor",
+        "stats": {
+            "gapl": 50,
+            "plty": 50,
+            "mode": 50,
+            "thpr": 0,
+            "shty": 50,
+            "ethi": 50
+        }
+    },
+    {
+        "name": "Tankist",
+        "stats": {
+            "gapl": 50,
+            "plty": 50,
+            "mode": 50,
+            "thpr": 50,
+            "shty": 100,
+            "ethi": 50
+        }
+    },
+    {
+        "name": "Heavenly",
+        "stats": {
+            "gapl": 50,
+            "plty": 50,
+            "mode": 50,
+            "thpr": 50,
+            "shty": 50,
+            "ethi": 100
+        }
+    },
+    {
+        "name": "Soulcrusher",
+        "stats": {
+            "gapl": 50,
+            "plty": 50,
+            "mode": 50,
+            "thpr": 50,
+            "shty": 50,
             "ethi": 0
         }
     },
     {
-        "name": "Isolated",
+        "name": "Basemaker",
         "stats": {
-            "gapl": 5,
-            "plty": 0,
-            "mode": 5,
-            "thpr": 5,
-            "shty": 5,
-            "ethi": 5
+            "gapl": 75,
+            "plty": 75,
+            "mode": 50,
+            "thpr": 50,
+            "shty": 60,
+            "ethi": 50
         }
     },
     {
-        "name": "Anarcho-Communism",
+        "name": "Duelist",
         "stats": {
-            "gapl": 10,
-            "plty": 30,
-            "mode": -20,
-            "thpr": 10,
-            "shty": 20,
-            "ethi": -5
+            "gapl": 0,
+            "plty": 20,
+            "mode": 50,
+            "thpr": 20,
+            "shty": 50,
+            "ethi": 50
         }
     },
+    {
+        "name": "Starblaster",
+        "stats": {
+            "gapl": 50,
+            "plty": 50,
+            "mode": 50,
+            "thpr": 50,
+            "shty": 50,
+            "ethi": 50
+        }
+    },
+    {
+        "name": "Speed Demon",
+        "stats": {
+            "gapl": 50,
+            "plty": 50,
+            "mode": 50,
+            "thpr": 50,
+            "shty": 0,
+            "ethi": 50
+        }
+    },
+    {
+        "name": "Mr. GG",
+        "stats": {
+            "gapl": 50,
+            "plty": 60,
+            "mode": 60,
+            "thpr": 50,
+            "shty": 50,
+            "ethi": 75
+        }
+    },
+    {
+        "name": "Racer",
+        "stats": {
+            "gapl": 50,
+            "plty": 40,
+            "mode": 50,
+            "thpr": 20,
+            "shty": 20,
+            "ethi": 50
+        }
+    },
+    {
+        "name": "Sin of Starblast",
+        "stats": {
+            "gapl": 0,
+            "plty": 0,
+            "mode": 50,
+            "thpr": 0,
+            "shty": 50,
+            "ethi": 0
+        }
+    },
+    {
+        "name": "Commander",
+        "stats": {
+            "gapl": 50,
+            "plty": 70,
+            "mode": 50,
+            "thpr": 80,
+            "shty": 50,
+            "ethi": 60
+        }
+    },
+    {
+        "name": "Speed Demon",
+        "stats": {
+            "gapl": 50,
+            "plty": 50,
+            "mode": 50,
+            "thpr": 50,
+            "shty": 0,
+            "ethi": 50
+        }
+    },
+    {
+        "name": "Helper",
+        "stats": {
+            "gapl": 60,
+            "plty": 80,
+            "mode": 50,
+            "thpr": 60,
+            "shty": 50,
+            "ethi": 60
+        }
+    },
+    {
+        "name": "Troller",
+        "stats": {
+            "gapl": 40,
+            "plty": 50,
+            "mode": 50,
+            "thpr": 30,
+            "shty": 50,
+            "ethi": 30
+        }
+    },
+    
     
 
     
